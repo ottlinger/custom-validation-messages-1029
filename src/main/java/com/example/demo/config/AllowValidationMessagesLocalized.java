af76eaaf877ccc4d2fd8fdb6a488174449eef868
Hugo Hirsch <github@hugo-hirsch.de>
2025-01-24 23:12:22 +0100

    Add i18n

---
+++ src/main/java/com/example/demo/config/AllowValidationMessagesLocalized.java
@@ -0,0 +1,26 @@
+package com.example.demo.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@Configuration
+public class AllowValidationMessagesLocalized {
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:myapp");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    // inject our own message source into the validation factory of Hibernate
+    @Bean
+    public LocalValidatorFactoryBean validator(MessageSource messageSource) {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource);
+        return bean;
+    }
+}
